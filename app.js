1ebd55828f6cfb560171dfe838da45b7e3b21578
nigg <thuse91@live.se>
2020-10-02 23:47:11 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+var content = document.querySelector(".content");
+
+function btn(){
+    if (content.style.display === "none"){
+      content.style.display = "block";
+    } else {
+      content.style.display = "none";
+    }
+  };
